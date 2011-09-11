9731188504b5a4fc679eb29e1853b37db0c463e1
Toda Takahiko <poepoemix@hotmail.com>
2011-09-11 11:06:37 +0900

    サーバーアクセス先をサクラサーバーからローカルホストに書き換え。

--- clientSrc/com/ttProject/udpp2p/client/instance/UdpP2pServer.java
+++ clientSrc/com/ttProject/udpp2p/client/instance/UdpP2pServer.java
@@ -30,7 +30,7 @@ public class UdpP2pServer implements Runnable {
 	/** 動作用ソケット */
 	private DatagramSocket socket;
 	/** 接続対象サーバー */
-	private SocketAddress server = new InetSocketAddress("49.212.39.17", 12345);
+	private SocketAddress server = new InetSocketAddress("localhost", 12345);
 	/** P2Pやりとり先ターゲット */
 	private SocketAddress target = null;
 	/** 処理アダプター */
